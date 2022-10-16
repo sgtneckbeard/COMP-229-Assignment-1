0919ba17c4bbf83e17f62d4607d4cb2aaae1ea62
sgtneckbeard <nathanheung@gmail.com>
2022-10-16 14:02:35 -0400

    attempted to use favicon, no luck yet

--- config/app.js
+++ config/app.js
@@ -4,6 +4,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
 
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
